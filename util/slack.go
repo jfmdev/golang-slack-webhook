da79df168a5b7a6ac404cbd9374db3c95196caa0
Jose <jfmdev@gmail.com>
2023-10-04 14:18:09 -0300

    Implement function to post messages to Slack

--- util/slack.go
+++ util/slack.go
@@ -1,10 +1,33 @@
 package util
 
 import (
+  "bytes"
+  "encoding/json"
   "fmt"
+  "net/http"
 )
 
-func PostMessageOnSlack(url string, message string) error {
-  fmt.Printf("TODO | Post to %s the message: %s \n", url, message)
+type SlackMessage struct {
+  Text string `json:"text"`
+}
+
+func PostMessageOnSlack(webhookUrl string, message string) error {
+  fmt.Println("HTTP JSON POST URL:", webhookUrl)
+
+  data := &SlackMessage{
+      Text: message}
+  jsonData, _ := json.Marshal(data)
+  byteData := []byte(string(jsonData))
+
+  request, reqError := http.NewRequest("POST", webhookUrl, bytes.NewBuffer(byteData))
+  request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+  client := &http.Client{}
+  response, reqError := client.Do(request)
+  if reqError != nil {
+    return reqError;
+  }
+  defer response.Body.Close()
+
   return nil
 }
